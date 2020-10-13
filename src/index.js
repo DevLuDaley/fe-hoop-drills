f674c6bbee5ac69f3063efc52d0de0c26aad40a4
Lu Daley <46202132+DevLuDaley@users.noreply.github.com>
2020-10-13 12:57:11 -0400

    create store && add thunk

--- src/index.js
+++ src/index.js
@@ -1,16 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
 // import * as serviceWorker from './serviceWorker';
 import App from './App';
 
+// compose == chain middleware functions
+let reducer = ''
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const enhancer = compose()
+
+let store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)))
+
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provide store={store}>
+  {/* <React.StrictMode> */}
+  {/* <Router> */}
     <App />
-  </React.StrictMode>,
+  {/* </React.StrictMode> */}
+  {/* </Router> */}
+  </Provide>
+  ,
   document.getElementById('root')
 );
 
