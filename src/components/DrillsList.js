eb958718e9275ba91e2bc47e0bd4cad67e4d16d8
Lu Daley <46202132+DevLuDaley@users.noreply.github.com>
2020-10-13 19:33:47 -0400

    add DrillList

---
+++ src/components/DrillsList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class DrillsList extends Component {
+    state = {  }
+    render() { 
+        return ( 
+        <div>
+        DrillsList
+        </div> );
+    }
+}
+ 
+export default DrillsList;
