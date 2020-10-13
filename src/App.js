52d7d78cad9921aa351f713364140538fd030c72
Lu Daley <46202132+DevLuDaley@users.noreply.github.com>
2020-10-13 11:54:39 -0400

    test api routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, { Component }from 'react';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+class App extends Component {
+  // state = {  }
+componentDidMount() {
+  fetch('http://localhost:3000/api/v1/routines',
+{ method: 'GET'}
+)
+.then(response => response.json())
+.then(jsonResponse =>
+  console.log('App -> componentDidMount -> jsonResponse', jsonResponse))
 }
 
+  render() { 
+    return ( 
+        <div className="App">
+        <p>
+        hi
+          {/* { jsonResponse } */}
+        </p>
+        </div>
+
+     );
+  }
+}
+ 
 export default App;
+// function App() {
+//   return (
+//     <div className="App">
+//     </div>
+//   );
+// }
+
+// export default App;
