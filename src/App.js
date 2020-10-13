95e9425a7bc03fd3f86baaef7758cc6d88d161ea
Lu Daley <46202132+DevLuDaley@users.noreply.github.com>
2020-10-13 19:32:56 -0400

    add RoutinesContainer

--- src/App.js
+++ src/App.js
@@ -1,30 +1,50 @@
-import React, { Component }from 'react';
+import React, { Component, Fragment }from 'react';
 import './App.css';
+// import {connect} from 'react-redux'
+// import {fetchRoutines} from './actions/fetchRoutines'
+import RoutinesContainer from './containers/RoutinesContainer'
+import DrillsContainer from './containers/DrillsContainer'
+
 class App extends Component {
   // state = {  }
 componentDidMount() {
-  fetch('http://localhost:3000/api/v1/routines',
-{ method: 'GET'}
-)
-.then(response => response.json())
-.then(jsonResponse =>
-  console.log('App -> componentDidMount -> jsonResponse', jsonResponse))
+  // this.props.fetchRoutines({ type: 'FETCH_ROUTINES', payload: {routine_name: 'name'}})
+//   fetch('http://localhost:3000/api/v1/routines',
+// { method: 'GET'}
+// )
+// .then(response => response.json())
+// .then(jsonResponse =>
+//   console.log('App -> componentDidMount -> jsonResponse', jsonResponse))
 }
+  // this.props.routines
+  // this.props.fetchRoutines
 
   render() { 
-    return ( 
+    return (
+      <Fragment>
         <div className="App">
-        <p>
-        hi
+        <RoutinesContainer/>
+        {/* <br/> */}
+        {/* <DrillsContainer/> */}
           {/* { jsonResponse } */}
-        </p>
         </div>
 
+      </Fragment> 
      );
   }
 }
- 
+//  const mapStateToProps = (state) = {
+//    return (
+//      routines: state.routines
+//    )
+
+//  }
+// ! call dispach on the return value of the 2nd argument (null,fetchRoutines) 
+// export default connect(null, {fetchRoutines})(App);
 export default App;
+// export default connect()(App);
+// export default connect(mapStateToProps, {fetchRoutines})(App);
+// export default connect(mapStateToProps, {fetchRoutines})(App);
 // function App() {
 //   return (
 //     <div className="App">
