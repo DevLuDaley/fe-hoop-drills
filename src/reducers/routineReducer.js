66308a499a7e3d10e75bb121a5ef9359e769c77a
devludaley <daleyluis@gmail.com>
2020-10-14 00:05:57 -0400

    + case: Add_Routine

--- src/reducers/routineReducer.js
+++ src/reducers/routineReducer.js
@@ -1,9 +1,12 @@
-export default function accountReducer( state = { routines: []}, action) {
+export default function accountReducer(state = {routines: []}, action) {
 // debugger;
     switch (action.type) {
         case 'FETCH_ROUTINES':
             // return state
             return {routines: action.payload}
+        case 'ADD_ROUTINE':
+            // console.log(state)
+            return {...state, routines:[...state.routines, action.payload]}
 
         default:
             return state
