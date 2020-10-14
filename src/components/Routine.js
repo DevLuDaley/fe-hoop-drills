7261ed45d1523c094537a2c1258c22b8d81a5438
devludaley <daleyluis@gmail.com>
2020-10-14 04:16:57 -0400

    add Redirect + remove bullets from ul

--- src/components/Routine.js
+++ src/components/Routine.js
@@ -1,13 +1,22 @@
 import React, { Fragment} from 'react'
+import {Redirect} from 'react-router-dom'
 
 const Routine = (props) => {
+    // console.log('props here =>',props.routine.id);
+    // console.log(`props`, props.id);
+    let routine = props.routines[props.match.params.id -1]
+    // console.log(routine);
+    // console.log(routine)
     return( 
     <Fragment>
-    <li key={props.id}>
+    <li key={props.routine} style={{listStyleType: 'none'}}>
+    {routine ? null : <Redirect to='/routines'/>}
+
         <h1>
-        {props.routine.routine_name}  
+        {routine ? routine.routine_name : null}
         </h1>
-        {props.routine.drills.map(drill => <h5>{drill.drill_name}</h5>  )
+        {/* console.log(routine) */}
+        {routine ? routine.drills.map(drill => <h5>{drill.drill_name}</h5>) : null
         }
     </li>
                 {/* <li key={routine.id}>
