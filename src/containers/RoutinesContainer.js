43c82a40faa6e3434a9592c819ced54403508076
devludaley <daleyluis@gmail.com>
2020-10-14 04:18:24 -0400

    + switch and update index/new/:id routes

--- src/containers/RoutinesContainer.js
+++ src/containers/RoutinesContainer.js
@@ -1,8 +1,9 @@
 import React, { Component, Fragment } from 'react';
 import {connect} from 'react-redux'
-import {Route} from 'react-router-dom'
+import {Route, Switch} from 'react-router-dom'
 import {fetchRoutines} from '../actions/fetchRoutines'
 import NewRoutineForm from '../components/NewRoutineForm'
+import Routine from '../components/Routine'
 import RoutinesList from '../components/RoutinesList'
 //'.components/NewRoutineForm'
 
@@ -15,15 +16,20 @@ class RoutinesContainer extends Component {
         return ( 
         <Fragment>
             <br></br>
-            <Route path='/routine/new' component={NewRoutineForm}/>
-            {/* <NewRoutineForm/> */}
-            <br></br>
-            <Route path='/routines' render={
-                () => <RoutinesList routines={this.props.routines}
-             /> } />
+            <Switch>
+
+            <Route path='/routines/new' component={NewRoutineForm}/>
+            <Route path='/routines/:id' render={
+                (routerProps) => <Routine {...routerProps} routines={this.props.routines}/>}/>
+            <Route exact path='/routines' render={
+                (routerProps) => <RoutinesList {...routerProps} routines={this.props.routines}
+             />}/>
+            </Switch>
         </Fragment> );
     }
 }
+
+//! redux store
 const mapStateToProps = state => {
     return(
         {
