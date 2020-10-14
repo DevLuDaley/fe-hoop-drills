3bcd3acd9cb0253a39332d86a97313a0b284af70
devludaley <daleyluis@gmail.com>
2020-10-14 13:52:09 -0400

    add id as hidden field in routine params

---
+++ src/actions/addRoutineDrill.js
@@ -0,0 +1,22 @@
+export const addRoutineDrill = (data) => {
+    // debugger
+    console.log(data)
+    // ! return dispatch so that we have access to dispatch function, thanks to thunk.
+    // ! thunk is allowing us to pass in dispatch to the thunk function that we have inside of our action creator
+
+    return(dispatch) => {
+        fetch(`http://localhost:3000/api/v1/routines/${data.id}`, {
+            headers: {
+                'Content-Type' : 'application/json',
+                'Accept' : 'application/json'
+            },
+            method: 'PATCH',
+            body: JSON.stringify(data)
+        })
+        .then(res => res.json())
+        .then(routine => dispatch({
+            type: 'ADD_ROUTINE_DRILL',
+            payload: routine
+        }))
+    }
+};
---
+++ src/components/NewRoutineDrillForm.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux'
+import {addRoutineDrill} from '../actions/addRoutineDrill'
+
+class NewRoutineDrillForm extends Component {
+    state = {  }
+
+    handleChange = (e)=> {
+        console.log(e.target);
+         this.setState({
+            [e.target.name]: e.target.value,
+            id: this.props.id['id']
+            // [e.target.drill_name]: e.target.value,
+            // [e.target.reps]: e.target.reps,
+            // [e.target.sets]: e.target.sets
+        })
+    }
+
+    handleSubmit = (e)=> {
+        e.preventDefault()
+        // console.log(e);
+        this.props.addRoutineDrill(this.state)
+        // e.target.children['name'].value = ''
+        // e.target.children['reps'].value = ''
+        // e.target.children['sets'].value = ''
+        // console.log(e.target);
+        // console.log(this.props)
+    }
+    render() { 
+        return (  
+        <form onSubmit={this.handleSubmit}>
+              <input type="hidden" placeholder='' value={this.props.id} name="id" onChange={this.handleChange}/>
+            <label>Name:</label> 
+              <input type="text" placeholder='enter new drill...' value={this.props.drill_name} name="drill_name" onChange={this.handleChange}/>
+            <label>Reps:</label> 
+              <input type="text" placeholder='enter reps...' value={this.props.reps} name="reps" onChange={this.handleChange}/>
+            <label> Sets:</label> 
+              <input type="text" placeholder='enter sets...' value={this.props.sets} name="sets" onChange={this.handleChange}/>
+                <button type="submit">Add Drill</button>
+        </form>
+        );
+    }
+}
+ 
+export default  connect(null, {addRoutineDrill}) (NewRoutineDrillForm);
