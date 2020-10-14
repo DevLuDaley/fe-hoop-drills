962c1eb4568bdc83111dd395490930f8111eb02f
devludaley <daleyluis@gmail.com>
2020-10-14 06:19:19 -0400

    add handle methods in routine

--- src/components/Routine.js
+++ src/components/Routine.js
@@ -7,22 +7,37 @@ const Routine = (props) => {
     let routine = props.routines[props.match.params.id -1]
     // console.log(routine);
     // console.log(routine)
+    handleChange = ()=> {
+        
+    }
+    handleSubmit = ()=> {
+
+    }
+handleChange = (params) => {
+    
+}
+
+
     return( 
     <Fragment>
-    <li key={props.routine} style={{listStyleType: 'none'}}>
-    {routine ? null : <Redirect to='/routines'/>}
+        <li key={props.routine} style={{listStyleType: 'none'}}>
+        {routine ? null : <Redirect to='/routines'/>}
 
-        <h1>
-        {routine ? routine.routine_name : null}
-        </h1>
-        {/* console.log(routine) */}
-        {routine ? routine.drills.map(drill => <h5>{drill.drill_name}</h5>) : null
-        }
-    </li>
-                {/* <li key={routine.id}>
-                    <h3>
-                    {routine.routine_name}
-                    </h3> */}
+            <h1>
+            {routine ? routine.routine_name : null}
+            </h1>
+            {/* console.log(routine) */}
+            {routine ? routine.drills.map(drill => <h5>{drill.drill_name}</h5>) : null
+            }
+        </li>
+        
+        Add a Drill to this Routine
+        <form onSubmit={this.handleSubmit}>
+                <label>Drill Name:</label> 
+                <input type="text" placeholder='enter new drill...' value={this.state.drill_name} name="drill_name" onChange={this.handleChange}/>
+                <button type="submit">Add Drill</button>
+        </form> 
+                
     </Fragment>
 
     )
