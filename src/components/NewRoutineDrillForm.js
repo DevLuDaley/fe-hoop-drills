1c8950b6c02d138cd3473d7d7a135928ee47eb7d
Lu Daley <46202132+DevLuDaley@users.noreply.github.com>
2020-10-15 17:58:18 -0400

    fix auto rerender bug

--- src/components/NewRoutineDrillForm.js
+++ src/components/NewRoutineDrillForm.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux'
+import {connect} from 'react-redux'
 import {addRoutineDrill} from '../actions/addRoutineDrill'
 
 class NewRoutineDrillForm extends Component {
-    state = {  }
+  constructor(props) {
+    super(props)
+    this.state = {
+      id: '',
+      drill_name: '',
+      reps: '',
+      sets: '',
+      }
+
+  }
 
     handleChange = (e)=> {
         console.log(e.target);
@@ -17,33 +26,39 @@ class NewRoutineDrillForm extends Component {
     }
 
     handleSubmit = (e)=> {
-        e.preventDefault()
-        // console.log(e);
+        e.preventDefault();
         this.props.addRoutineDrill(this.state)
-         this.setState({
-            [e.target.name]: e.target.value,
-            id: this.props.routine['id']})
-        // e.target.children['name'].value = ''
-        // e.target.children['reps'].value = ''
-        // e.target.children['sets'].value = ''
-        // console.log(e.target);
-        // console.log(this.props)
-        
+            this.setState({
+      id: '',
+      drill_name: '',
+      reps: '',
+      sets: '',
+      })
+      //! formData object/hash
+        // let formData  = { 
+        //   [e.target.drill_name]: this.state.drill_name,
+        //   [e.target.reps]: this.state.reps,
+        //   [e.target.sets]: this.state.sets,
+        //   id: this.props.routine['id']
+        //  }
     }
     render() { 
         return (  
+          <div>
         <form onSubmit={this.handleSubmit}>
-              <input type="hidden" placeholder='' value={this.props.id} name="id" onChange={this.handleChange}/>
+              <input type="hidden" placeholder='' value={this.state.id} name="id" onChange={this.handleChange}/>
             <label>Name:</label> 
-              <input type="text" placeholder='enter new drill...' value={this.props.drill_name} name="drill_name" onChange={this.handleChange}/>
+              <input type="text" placeholder='enter new drill...' value={this.state.drill_name} name="drill_name" onChange={this.handleChange}/>
             <label>Reps:</label> 
-              <input type="text" placeholder='enter reps...' value={this.props.reps} name="reps" onChange={this.handleChange}/>
+              <input type="text" placeholder='enter reps...' value={this.state.reps} name="reps" onChange={this.handleChange}/>
             <label> Sets:</label> 
-              <input type="text" placeholder='enter sets...' value={this.props.sets} name="sets" onChange={this.handleChange}/>
+              <input type="text" placeholder='enter sets...' value={this.state.sets} name="sets" onChange={this.handleChange}/>
                 <button type="submit">Add Drill</button>
         </form>
+        </div>
         );
-    }
+      }
+      // debugger
 }
  
-export default  connect(null, {addRoutineDrill}) (NewRoutineDrillForm);
+export default connect(null, {addRoutineDrill}) (NewRoutineDrillForm);
