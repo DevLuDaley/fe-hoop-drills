97d613bf550a930acc4906402caef1f6a6cebea1
Lu Daley <46202132+DevLuDaley@users.noreply.github.com>
2020-10-15 18:00:15 -0400

    update reducer add_routine_drill action

--- src/components/Routine.js
+++ src/components/Routine.js
@@ -1,4 +1,4 @@
-import React, { Fragment} from 'react'
+import React, { Fragment } from 'react'
 import {Redirect} from 'react-router-dom'
 import NewRoutineDrillForm from '../components/NewRoutineDrillForm'
 
@@ -7,17 +7,17 @@ const Routine = (props) => {
     // console.log(`props`, props.id);
     // let routine = props.routines[props.match.params.id -1]
     let routine = props.routines.filter(routine => routine.id == props.match.params.id)[0]
+    // console.log('Routine -> routine', routine);
     // console.log(routine);
-    // console.log(routine)
-    
-
+    // console.log('ROUTINE => ', routine)
 
     return( 
     <Fragment>
         <li className='routine-li' key={props.routine} style={{listStyleType: 'none'}}>
-        {routine ? null : <Redirect to='/routines'/>}
+        {/* {routine ? null : <Redirect to='/routines'/>} */}
 
             <h1>
+            {/* {routine.routine_name} */}
             {routine ? routine.routine_name : null}
             </h1>
             <h3 className='drills-list'>Drills List</h3>
@@ -25,9 +25,8 @@ const Routine = (props) => {
             }
         </li>
         
-   <NewRoutineDrillForm routine={routine} 
-//    id={props.routines[props.match.params.id -1]}
-   />
+   <NewRoutineDrillForm routine={routine} />
+   {/* id={props.routines[props.match.params.id -1]} */}
     </Fragment>
 
     )
--- src/reducers/routineReducer.js
+++ src/reducers/routineReducer.js
@@ -1,4 +1,4 @@
-export default function accountReducer(state = {routines: []}, action) {
+ export default function routineReducer(state = {routines: []}, action) {
 // debugger;
     switch (action.type) {
         case 'FETCH_ROUTINES':
@@ -7,18 +7,32 @@ export default function accountReducer(state = {routines: []}, action) {
         case 'ADD_ROUTINE':
             return {...state, routines:[...state.routines, action.payload]}
         case 'ADD_ROUTINE_DRILL':
-            let routinesDrills =  state.routines.map(routine => {
+            // let routineId = [action.payload.id -1]
+            let routineDrills =  state.routines.map(routine => {
                 if (routine.id === action.payload.id) {
                     return action.payload
-                } else {
-                    return routine
-                }
-            })
-            return {...state, routines:[...state.routines, action.payload]}
+                    } else {
+                        return routine
+                    }
+                  }
+                )
+            return {...state, routines: routineDrills}
 
         default:
+            console.log('accountReducer -> state', state);
             return state
     }
+
+                    // console.log('accountReducer -> ...state', [...state]);
+                // console.log('accountReducer -> state', state);
+                // console.log('accountReducer -> state.routine', state.routines[action.payload.id -1]);
+                // console.log('accountReducer -> state.routines[routineId]', state.routines[routineId]);
+                // console.log('accountReducer -> state.routines[routineId].drills', state.routines[routineId].drills);
+                // console.log('accountReducer -> action.payload', action.payload);
+                // return {...state, routine: [state.routines[routineId]].drills} //, action.payload]}
+                // } else {
+                    // return action.payload //routine
+        // }
 
     //Routine.all.length || Routine.all.count
 // return state
