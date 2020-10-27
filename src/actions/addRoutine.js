5da03eb0c83a6e943f0d365e1dd8bb969cde1ceb
devludaley <daleyluis@gmail.com>
2020-10-26 21:58:33 -0400

    update baseUrl

--- src/actions/addRoutine.js
+++ src/actions/addRoutine.js
@@ -2,20 +2,21 @@ export const addRoutine = (data) => {
 // debugger
 // ! return dispatch so that we have access to dispatch function, thanks to thunk.
 // ! thunk is allowing us to pass in dispatch to the thunk function that we have inside of our action creator
+    let baseUrl = 'http://localhost:3000/api/v1/routines/'
 
-    return(dispatch) => {
-fetch('http://localhost:3000/api/v1/routines', {
-    headers: {
-        'Content-Type' : 'application/json',
-        'Accept' : 'application/json'
-    },
-    method: 'POST',
-    body: JSON.stringify(data)
-})
-.then(res => res.json())
-.then(routine => dispatch({
-    type: 'ADD_ROUTINE', 
-    payload: routine
-}))
-    }
+        return(dispatch) => {
+            fetch(baseUrl, {
+                headers: {
+                    'Content-Type' : 'application/json',
+                    'Accept' : 'application/json'
+                },
+                method: 'POST',
+                body: JSON.stringify(data)
+            })
+            .then(res => res.json())
+            .then(routine => dispatch({
+                type: 'ADD_ROUTINE', 
+                payload: routine
+            }))
+            }
 };
