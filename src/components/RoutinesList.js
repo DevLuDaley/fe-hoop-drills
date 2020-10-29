117399043e687894c232b241ea52d1e67841cf71
devludaley <daleyluis@gmail.com>
2020-10-29 02:13:43 -0400

    feature-routines-list-alpha-sort

--- src/components/RoutinesList.js
+++ src/components/RoutinesList.js
@@ -1,25 +1,47 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useState, useEffect } from 'react'
 import {Link} from 'react-router-dom';
-import Routine from '../components/Routine'
+// import Routine from '../components/Routine'
 
 
-const RoutinesList = (props)  => {
-        return (
+const RoutinesList = props => {
+    const [stateRoutines, setStateRoutines] = useState([])
+    //! [[],fn]
+    // ! use state as a low cost lifecycle methods
+        useEffect(() => {
+                setStateRoutines(props.routines);
+            }, [props.routines]);
+    var ascSort = (a, b) =>
+    {
+            return a.routine_name === b.routine_name ? 0 : a.routine_name > b.routine_name ? 1 : -1
+    }
+
+    var descSort = (a, b) =>
+    {
+            return a.routine_name === b.routine_name ? 0 : a.routine_name < b.routine_name ? 1 : -1
+    }
+
+         return (
         <Fragment>
-        {console.log('RoutinesList -> props.routines', props.routines)}
-            {props.routines && props.routines.map(routine => 
-            <ul key={routine.id} className='routines-list'>
-                  <li key={routine.id}>
-                      <Link to={`/routines/${routine.id}`}> 
-                            {routine.routine_name} 
-                            <button>
-                                {routine.drills ? routine.drills.length : null}
-                            </button> 
-                      </Link> 
-                  </li> 
+            <button key={99} onClick={() => setStateRoutines([...stateRoutines].sort(ascSort))}
+            >sort asc</button>   
+            <button key={98} onClick={() => setStateRoutines([...stateRoutines].sort(descSort))}
+            >sort desc</button>
+            <ul key={props.id} id='list-of-routines' className='routines-list'>
+                {
+                    
+                  stateRoutines && stateRoutines.map(
+                    routine => 
+                        <li key={routine.id}>
+                            <Link to={`/routines/${routine.id}`}> 
+                                    {routine.routine_name} 
+                                    <button>
+                                        {routine.drills ? routine.drills.length : null}
+                                    </button> 
+                            </Link> 
+                        </li> 
+                    )
+                }
             </ul>
-            )
-            }
         </Fragment>
         );
 }
